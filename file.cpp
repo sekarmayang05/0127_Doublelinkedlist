923a10b5459dea717b6de8cacbb6a77fa8d7418b
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-05-15 15:07:21 +0700

    membuat class node

---
+++ file.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node
+{
+public:
+    int noMhs;
+    Node *next;
+    Node *prev;
+};
+
+class DoubleLinkedlist
+{
+private:
+    Node *START;
+}
