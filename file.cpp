37b74cf9a5b19480f0e3865b067429a4b3fd3f7b
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-05-15 15:27:11 +0700

    membuat class double linked list

--- file.cpp
+++ file.cpp
@@ -14,4 +14,42 @@ class DoubleLinkedlist
 {
 private:
     Node *START;
-}
+public:
+    DoubleLinkedlist()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nEnter the roll number of the student:";
+        cin >> nim;
+
+        // step 1 : Alokasi memori untuk new node
+        Node *newNode = new Node();
+
+        // step 2 : Asign value to the fields 
+        newNode ->noMhs = nim;
+
+        // step 3 : Insert at begining if list is empty or nim is smallest
+        if (START == NULL || nim <= START -> noMhs)
+        {
+            if (START != NULL && nim == START -> noMhs)
+            {
+                cout << "\nDuplicate number not allowed" << endl;
+                return;
+            }
+            // step 4 : newNode.next = START
+            newNode->next =START;
+
+            //step 5 : START.prev = newNode (if START exixst)
+            if (START != NULL)
+                START -> prev = newNode;
+        
+            
+        }
+    }
+       
+    
+};
