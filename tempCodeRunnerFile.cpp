8b703a1f91d2a05ce1ed1801aa527df6eb58009f
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-05-15 18:22:39 +0700

    Press Enter to continue

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,19 +1,10 @@
-switch (choice)
-        {
-        case '1':
-            list.addNode();
-            break;
-        case '2':
-            list.hapus();
-            break;
-        case '3':
-            list.traverse();
-            break;
-        case '4':
-            list.revtraverse();
-            break;
-        case '5':
-            list.searchData();
-            break;
-        case '6':
-            return 0;
+ default:
+            cout << "Invalid option\n";
+        }
+        cout << "\nPress Enter to continue...";
+        cin.ignore();
+        cin.get();
+        cout << endl;
+        system("clear");
+    }while (choice != '6');
+}
