f72e0b6ab3e16817816b38ed51180d3b6fe0a333
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-05-15 18:08:28 +0700

    insert last node

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,3 +1,4 @@
-// step 9 : Insert between current and current->next
-        newNode->next = current->next;   // step 9a : newNode.next = current.next
-        newNode->prev = current;        // step 9b : newNode.prev = current
+//insert last node 
+        if (current->next != NULL)
+            current->next->prev = newNode; // step 9c : current.next.prev = newNode
+        current ->next = newNode ;   // step 9d : current.next = newNode
