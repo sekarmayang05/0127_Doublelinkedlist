75b45112dfbd90df9f6adedfbd21c173ee6dc2e9
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-05-15 18:14:13 +0700

    void traverse

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,51 +1,23 @@
-void hapus()
+void traverse ()
     {
         if (START == NULL)
         {
             cout << "\nList is empty" << endl;
             return;
-        
         }
-        
-        cout << "\nMasukkan NIM  yang akan didelated";
-        int rollNo;
-        cin >> rollNo;
 
-        Node *current = START;
+        // step 1 : Mark first node as currentNode
+        Node *currentNode = START;
 
-        //step 1 : Transverse the list to find the node
-        while (current != NULL && current ->noMhs != rollNo)
+        // STEP 2 : Repeat until currentNode == NULL 
+        cout << "\nRecords in ascending order of roll number aare:\n";
+        int i = 0;
+        while (currentNode != NULL)
         {
-            current = current->next;
+            cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
+
+            // step 3 : Move to next node
+            currentNode = currentNode->next;
+            i++;
         }
-
-        if (current == NULL)
-        {
-            cout << "Record tot found " << endl;
-            return;
-        }
-
-        //step 2 : if node is the begining 
-        if (current = START)
-        {
-            START = current->next; // step 2a: START.next
-            if (START == START)
-            {
-              START->prev = NULL; // STEP 2b : START.prev = NULL  
-
-            }
-        }
-        else
-        {
-            //step 3 : link previous node to next of current
-            current->prev->next = current->next;
-
-            // step 4 : if current is not the last node
-            if (current->next != NULL)
-                current->next->prev = current->prev;
-        }
-
-        // step 5 : Delete the node
-        delete current;
-        cout << "Record with roll number" << rollNo << "deleted" << endl;
     }
