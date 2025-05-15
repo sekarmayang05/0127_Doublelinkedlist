0e6c865612267fbdacc6e3862b316734807ccaf2
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-05-15 18:18:41 +0700

    menambahkan menu utama untuk menagtur data di double linked list

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,31 +1,17 @@
- void searchData()
+int main()
+{
+    DoubleLinkedlist list;
+    char choice;
+
+    do 
     {
+        cout << "\nMenu:\n";
+        cout << "1. Add Record\n";
+        cout << "2. Delete Record\n";
+        cout << "3. View Ascending\n";
+        cout << "4. View Descending\n";
+        cout << "5. Search Record\n";
+        cout << "6. Exit\n";
+        cout << "7. Enter your choice: ";
 
- if (START == NULL)
-        {
-            cout << "\nList is empty "<< endl;
-            return;
-        }
-
-        int rollNo;
-        cout << "\nEnter thr roll number to serch: ";
-        cin >> rollNo;
-
-        Node *current = START;
-
-        //step 1: Traverse to find matching roll number
-        while (current != NULL && current->noMhs != rollNo)
-            current = current->next;
-        
-        // step 2 : Output result
-        if (current == NULL)
-        {
-            cout << "Record not found\n";
-        }
-        else
-        {
-            cout << "Record found\n";
-            cout << "Roll Number: "<< current->noMhs << endl;
-        }
-    }  
-};
+        cin >> choice;
