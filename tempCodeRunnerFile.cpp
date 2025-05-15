7e6be9d8b925d0033fafb9ece488faaba88a28ce
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-05-15 18:15:15 +0700

    void revtraverse

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,23 +1,28 @@
-void traverse ()
+void revtraverse()
     {
         if (START == NULL)
         {
-            cout << "\nList is empty" << endl;
+            cout << "\nList is empty "<< endl;
             return;
         }
 
-        // step 1 : Mark first node as currentNode
+        // step 1 : Move to last node
         Node *currentNode = START;
-
-        // STEP 2 : Repeat until currentNode == NULL 
-        cout << "\nRecords in ascending order of roll number aare:\n";
         int i = 0;
+        while (currentNode != NULL)
+        {
+            currentNode = currentNode->next;
+            i++;
+        }
+
+        // step 2 : Traverse backward
+        cout << "\nRecords in discending order of roll number are :\n";
         while (currentNode != NULL)
         {
             cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
 
-            // step 3 : Move to next node
-            currentNode = currentNode->next;
-            i++;
+            // step 3 : Move to previous node
+            currentNode = currentNode->prev;
+            i--;
         }
     }
